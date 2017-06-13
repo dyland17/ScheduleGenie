62c6e61bacfd98a887adb1e951c806f2aca542f1
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-13 17:36:27 -0400

    TimeSheet not made yet but, started DayTracker that utilizes TimeSheet.

---
+++ ScheduleGenie/src/dewalddylan/data/DayTracker.java
@@ -0,0 +1,17 @@
+package dewalddylan.data;
+
+import java.util.ArrayList;
+
+public class DayTracker {
+	private ArrayList<TimeSheet> daySheet;
+	public DayTracker(){
+		daySheet = new ArrayList<TimeSheet>();
+	}
+	
+	//public getTimeSheetByDay(Day day){
+		
+	//}
+	//public setTimeSheetByDay(Day day, Time time){
+		
+	//}
+}

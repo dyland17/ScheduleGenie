620dafe4c0df76dc766ed3a4e2215111d6e033e6
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-13 22:02:18 -0400

    Time has getter and setters now.

--- ScheduleGenie/src/dewalddylan/data/Time.java
+++ ScheduleGenie/src/dewalddylan/data/Time.java
@@ -1,11 +1,33 @@
 package dewalddylan.data;
 
+import dewalddylan.data.enumerations.TimeOfDay;
+
 public class Time {
 	private int hour;
 	private int minute;
-	public Time(int hour, int min){
+	private TimeOfDay timeOfDay;
+	public Time(int hour, int min, TimeOfDay timeOfDay){
 		this.hour = hour;
 		minute = min;
+		this.timeOfDay = timeOfDay;
+	}
+	public int getHour() {
+		return hour;
+	}
+	public void setHour(int hour) {
+		this.hour = hour;
+	}
+	public int getMinute() {
+		return minute;
+	}
+	public void setMinute(int minute) {
+		this.minute = minute;
+	}
+	public TimeOfDay getTimeOfDay() {
+		return timeOfDay;
+	}
+	public void setTimeOfDay(TimeOfDay timeOfDay) {
+		this.timeOfDay = timeOfDay;
 	}
 	
 }

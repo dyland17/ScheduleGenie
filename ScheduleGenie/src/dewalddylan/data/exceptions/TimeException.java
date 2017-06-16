7c6dbfc0fe2c1172046877602b8b7b518515850b
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-16 14:33:33 -0400

    Changing the way TimeException works.

--- ScheduleGenie/src/dewalddylan/data/exceptions/TimeException.java
+++ ScheduleGenie/src/dewalddylan/data/exceptions/TimeException.java
@@ -3,10 +3,24 @@ package dewalddylan.data.exceptions;
 import javax.swing.JOptionPane;
 
 public class TimeException extends Exception{
+	private int hour, minute;
+	private String note = "";
+	public static final String EHOUR = "The hour you have entered is wrong. Try again.";
+	public static final String EMINUTE = "The minute you have entered is wrong. Try again.";
 	public TimeException(int hour, int minute){
-		JOptionPane.showMessageDialog(null, "The hour: " + hour + " or the minute: " + minute + " you entered is not possible. Try again.");
+		this.hour = hour;
+		this.minute = minute;
 	}
 	public TimeException(int hour, int minute, String note){
-		JOptionPane.showMessageDialog(null, note+ ":\n" + "The hour: " + hour + " or the minute: " + minute + " you entered is not possible. Try again.");
+		this(hour,minute);
+		this.note = note;
+	}
+	public void showMessage(){
+		if(note.equals("")){
+			JOptionPane.showMessageDialog(null, "The hour: " + hour + " or the minute: " + minute + " you entered is not possible. Try again.");
+		}
+		else{
+			JOptionPane.showMessageDialog(null, note + ":\n" + "The hour: " + hour + " or the minute: " + minute);
+		}
 	}
 }

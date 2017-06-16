5e02afeec5ea2729106bd8ebc6df694d58df339f
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-16 13:58:13 -0400

    Added an extra constructor for information if it is provided.

--- ScheduleGenie/src/dewalddylan/data/exceptions/TimeException.java
+++ ScheduleGenie/src/dewalddylan/data/exceptions/TimeException.java
@@ -6,4 +6,7 @@ public class TimeException extends Exception{
 	public TimeException(int hour, int minute){
 		JOptionPane.showMessageDialog(null, "The hour: " + hour + " or the minute: " + minute + " you entered is not possible. Try again.");
 	}
+	public TimeException(int hour, int minute, String note){
+		JOptionPane.showMessageDialog(null, note+ ":\n" + "The hour: " + hour + " or the minute: " + minute + " you entered is not possible. Try again.");
+	}
 }

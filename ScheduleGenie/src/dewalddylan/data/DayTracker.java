4ebe8aaddc4963dbdb1f87a33031ceb6f79dd173
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-16 14:33:03 -0400

    Made some changes to DayTracker.

--- ScheduleGenie/src/dewalddylan/data/DayTracker.java
+++ ScheduleGenie/src/dewalddylan/data/DayTracker.java
@@ -2,16 +2,26 @@ package dewalddylan.data;
 
 import java.util.ArrayList;
 
+import dewalddylan.data.enumerations.WorkDay;
+
 public class DayTracker {
 	private ArrayList<TimeSheet> daySheet;
 	public DayTracker(){
 		daySheet = new ArrayList<TimeSheet>();
 	}
 	
-	//public getTimeSheetByDay(Day day){
-		
-	//}
+	public TimeSheet getTimeSheetByDay(WorkDay day){
+		for(int i = 0; i < daySheet.size(); i++){
+			if(day == daySheet.get(i).getWorkDay())
+				return daySheet.get(i);
+		}
+		return null;
+	}
 	//public setTimeSheetByDay(Day day, Time time){
 		
 	//}
+	
+	public void addNewTimeSheet(TimeSheet sheet){
+		daySheet.add(sheet);
+	}
 }

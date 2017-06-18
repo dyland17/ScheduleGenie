4081f7ba5b097c14db82a650b8e0f51a07312463
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-18 14:09:53 -0400

    Completed the look for newEmployeeScreen in Window Class.

--- ScheduleGenie/src/dewalddylan/schedulegenie/gui/Window.java
+++ ScheduleGenie/src/dewalddylan/schedulegenie/gui/Window.java
@@ -1,32 +1,46 @@
 package dewalddylan.schedulegenie.gui;
 
+import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
-
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
-
 import dewalddylan.data.enumerations.ScreenType;
 
-public class Window {
-	private JFrame window;
-	private JTextField tfEmployeeName;
-	private JTextField tfAge;
+public abstract class Window {
+	//All GUI components
+	protected JFrame window;
+	protected JTextField tfEmployeeName;
+	protected JTextField tfAge;
+	protected JTextField tfTitle;
+	protected JTextField tfTotalHours;
+	protected JButton butCancel;
+	protected JButton butCreate;
+	//Size fields
 	private final int  TFSIZE = 10;
+	private final Font BOLDFONT = new Font(Font.SANS_SERIF,Font.BOLD,18);
 	private final Font TEXTFONT = new Font(Font.SANS_SERIF,Font.PLAIN,18);
-	private final Dimension EMPLOYEESCREEN = new Dimension(480,520);
-	public static final String NEWEMPLOYEE = "NEWEMPLOYEE"; 
+	private final Dimension EMPLOYEESCREEN = new Dimension(420,400);
+	private final Dimension JPANELOVERALLSIZE = new Dimension(320,110);
+	private final Dimension JPANELINNERSIZE = new Dimension(320,140);
+	private final Dimension BUTTONSIZE = new Dimension(140, 60);
 	
-	public Window(String name,ScreenType type){
+	public  Window(String name,ScreenType type){
 		setupJFrame(name,type);
 	}
 	private void setupJFrame(String name, ScreenType type) {
 		window = new JFrame(name);
 		tfEmployeeName = new JTextField(TFSIZE);
 		tfAge = new JTextField(TFSIZE);
+		tfTitle = new JTextField(TFSIZE);
+		tfTotalHours = new JTextField(TFSIZE);
+		butCancel = new JButton("Cancel");
+		butCreate = new JButton("Create");
 		if(type == ScreenType.NEWEMPLOYEE)
 			createNewEmployeeScreen();
 			
@@ -37,17 +51,15 @@ public class Window {
 		
 		Container c = window.getContentPane();
 		c.setLayout(new FlowLayout());
-		JLabel jlRequired = new JLabel("Required:");
-		jlRequired.setFont(TEXTFONT);
-		c.add(jlRequired);
-		JLabel jlEmployeeName = new JLabel("Employee's name:");
-		jlEmployeeName.setFont(TEXTFONT);
-		c.add(jlEmployeeName);
-		c.add(tfEmployeeName);
-		JLabel jlAge = new JLabel("Employee's age:");
-		jlAge.setFont(TEXTFONT);
-		c.add(jlAge);
-		c.add(tfAge);
+		
+		JPanel jpReqPanel = this.setupRequiredGUIForNewEmployeeScreen();
+		c.add(jpReqPanel);
+		
+		JPanel jpOptPanel = this.setupOptionalGUIForNewEmployeeScreen();
+		c.add(jpOptPanel);
+		
+		JPanel jpButPanel = this.setupButtonGUIForNewEmployeeScreen();
+		c.add(jpButPanel);
 		
 		window.requestFocusInWindow();
 		window.pack();
@@ -55,7 +67,68 @@ public class Window {
 		window.setVisible(true);
 		
 	}
+	
+	private JPanel setupRequiredGUIForNewEmployeeScreen(){
+		JPanel jpReqPanel = new JPanel();
+		jpReqPanel.setPreferredSize(JPANELOVERALLSIZE);
+		JLabel jlRequired = new JLabel("<HTML><U>Required:</U></HTML>");
+		jlRequired.setFont(BOLDFONT);
+		jpReqPanel.add(jlRequired,BorderLayout.NORTH);
+		
+		JPanel jpReqInnerPanel = new JPanel();
+		jpReqInnerPanel.setLayout(new FlowLayout());
+		jpReqInnerPanel.setPreferredSize(JPANELINNERSIZE);
+		JLabel jlEmployeeName = new JLabel("Employee's name:");
+		jlEmployeeName.setFont(TEXTFONT);
+		jpReqInnerPanel.add(jlEmployeeName);
+		jpReqInnerPanel.add(tfEmployeeName);
+		
+		JLabel jlAge = new JLabel("Employee's age:");
+		jlAge.setFont(TEXTFONT);
+		jpReqInnerPanel.add(jlAge);
+		jpReqInnerPanel.add(tfAge);
+		jpReqPanel.add(jpReqInnerPanel,BorderLayout.SOUTH);
+		return jpReqPanel;
+	}
+	
+	private JPanel setupOptionalGUIForNewEmployeeScreen(){
+		JPanel jpOptPanelOuter = new JPanel();
+		jpOptPanelOuter.setPreferredSize(JPANELOVERALLSIZE);
+		
+		JLabel jlOptional = new JLabel("<HTML><U>Optional:</U></HTML>");
+		jlOptional.setFont(BOLDFONT);
+		jpOptPanelOuter.add(jlOptional, BorderLayout.NORTH);
+		
+		JPanel jpOptFieldsPanel = new JPanel();
+		jpOptFieldsPanel.setPreferredSize(JPANELINNERSIZE);
+		jpOptFieldsPanel.setLayout(new FlowLayout());
+		
+		JLabel jlTitle = new JLabel("Employee's title: ");
+		jlTitle.setFont(TEXTFONT);
+		jpOptFieldsPanel.add(jlTitle);
+		jpOptFieldsPanel.add(tfTitle);
+		
+		JLabel jlTotalHours = new JLabel("Employee's total hours: ");
+		jlTotalHours.setFont(TEXTFONT);
+		jpOptFieldsPanel.add(jlTotalHours);
+		jpOptFieldsPanel.add(tfTotalHours);
+		jpOptPanelOuter.add(jpOptFieldsPanel, BorderLayout.SOUTH);
+		return jpOptPanelOuter;
+		
+	}
+	private JPanel setupButtonGUIForNewEmployeeScreen() {
+		JPanel jpButPanel = new JPanel();
+		jpButPanel.setPreferredSize(JPANELOVERALLSIZE);
+		
+		butCancel.setPreferredSize(BUTTONSIZE);
+		jpButPanel.add(butCancel, BorderLayout.WEST);
+		
+		butCreate.setPreferredSize(BUTTONSIZE);
+		jpButPanel.add(butCreate, BorderLayout.EAST);
+		return jpButPanel;
+		
+	}
 	public static void main(String[] args){
-		new Window("Add new Employee",ScreenType.NEWEMPLOYEE);
+		new NewEmployeeScreen("Add new Employee",ScreenType.NEWEMPLOYEE);
 	}
 }

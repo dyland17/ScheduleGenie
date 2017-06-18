35adf1d7371c8879fe5affc5642f2814324c29e9
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-18 14:24:49 -0400

    Starting to write functionality for newEmployeeScreen.

--- ScheduleGenie/src/dewalddylan/schedulegenie/gui/NewEmployeeScreen.java
+++ ScheduleGenie/src/dewalddylan/schedulegenie/gui/NewEmployeeScreen.java
@@ -1,11 +1,36 @@
 package dewalddylan.schedulegenie.gui;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import dewalddylan.data.Employee;
 import dewalddylan.data.enumerations.ScreenType;
 
-public class NewEmployeeScreen extends Window{
+public class NewEmployeeScreen extends Window implements ActionListener{
 
+	
 	public NewEmployeeScreen(String name, ScreenType type) {
 		super(name, type);
+		butCancel.addActionListener(this);
+		butCreate.addActionListener(this);
+	}
+	//Creation button functionality.
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource() == butCancel){
+			window.dispose();
+		}
+		else if(e.getSource() == butCreate){
+			Employee newEmployee = new Employee(tfEmployeeName.getText(),Integer.parseInt(tfAge.getText()),Integer.parseInt(tfTotalHours.getText()),tfTitle.getText());
+			printEmployeeInfo(newEmployee);
+		}
+	}
+	//Just to Debug if an employee's information is correct.
+	private void printEmployeeInfo(Employee newEmployee) {
+		System.out.println("Name: " + newEmployee.getName());
+		System.out.println("Age: " + newEmployee.getAge());
+		System.out.println("Title: "  + newEmployee.getTitle());
+		System.out.println("TotalHours: " + newEmployee.getTotalHours());
 	}
 	
 }

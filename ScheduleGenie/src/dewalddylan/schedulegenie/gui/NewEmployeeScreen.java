a0ee82fd95b3ce7ae3b1e6172c130f35e0844421
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-19 11:54:59 -0400

    Modified this file to meet changes with Employee class.

--- ScheduleGenie/src/dewalddylan/schedulegenie/gui/NewEmployeeScreen.java
+++ ScheduleGenie/src/dewalddylan/schedulegenie/gui/NewEmployeeScreen.java
@@ -21,13 +21,14 @@ public class NewEmployeeScreen extends Window implements ActionListener{
 			window.dispose();
 		}
 		else if(e.getSource() == butCreate){
-			Employee newEmployee = new Employee(tfEmployeeName.getText(),Integer.parseInt(tfAge.getText()),Integer.parseInt(tfTotalHours.getText()),tfTitle.getText());
+			Employee newEmployee = new Employee(tfEmployeeFirstName.getText(),tfEmployeeLastName.getText(),Integer.parseInt(tfAge.getText()),Integer.parseInt(tfTotalHours.getText()),tfTitle.getText());
 			printEmployeeInfo(newEmployee);
 		}
 	}
 	//Just to Debug if an employee's information is correct.
 	private void printEmployeeInfo(Employee newEmployee) {
-		System.out.println("Name: " + newEmployee.getName());
+		System.out.println("First Name: " + newEmployee.getFirstName());
+		System.out.println("Last Name: " + newEmployee.getLastName());
 		System.out.println("Age: " + newEmployee.getAge());
 		System.out.println("Title: "  + newEmployee.getTitle());
 		System.out.println("TotalHours: " + newEmployee.getTotalHours());

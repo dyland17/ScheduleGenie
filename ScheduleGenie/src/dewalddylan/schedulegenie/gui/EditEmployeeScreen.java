a1df1151929a68a4ba8df6f4b04e8fb667a7efa2
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-20 20:16:15 -0400

    Almost implemented but needs a reference to original screen.

---
+++ ScheduleGenie/src/dewalddylan/schedulegenie/gui/EditEmployeeScreen.java
@@ -0,0 +1,44 @@
+package dewalddylan.schedulegenie.gui;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import dewalddylan.data.Employee;
+import dewalddylan.data.enumerations.ScreenType;
+
+public class EditEmployeeScreen extends Window{
+	private Employee employeeToBeEdited;
+	public EditEmployeeScreen(String name, Employee employee) {
+		super(name, ScreenType.EDITEMPLOYEE);
+		employeeToBeEdited = employee;
+		butUpdate.addActionListener(this);
+		setupTfInfo();
+	}
+	
+
+	private void setupTfInfo() {
+		tfEmployeeFirstName.setText(employeeToBeEdited.getFirstName());
+		tfEmployeeLastName.setText(employeeToBeEdited.getLastName());
+		tfAge.setText(String.valueOf(employeeToBeEdited.getAge()));
+		tfTitle.setText(employeeToBeEdited.getTitle());
+		tfTotalHours.setText(String.valueOf(employeeToBeEdited.getTotalHours()));
+		
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		super.actionPerformed(e);
+		Employee originalEmployee = employeeToBeEdited.copy();
+		if(e.getSource() == butUpdate){
+			employeeToBeEdited.setFirstName(tfEmployeeFirstName.getText());
+			employeeToBeEdited.setLastName(tfEmployeeLastName.getText());
+			employeeToBeEdited.setAge(Integer.parseInt(tfAge.getText()));
+			employeeToBeEdited.setTitle(tfTitle.getText());
+			employeeToBeEdited.setTotalHours(Integer.parseInt(tfTotalHours.getText()));
+			
+		}
+		
+	}
+
+}

86ff65c8c18ff0b1e6a020f146abbbef2fcd417f
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-27 20:10:26 -0400

    Edited the messages that got displayed in TimeException.

--- ScheduleGenie/src/dewalddylan/data/exceptions/TimeException.java
+++ ScheduleGenie/src/dewalddylan/data/exceptions/TimeException.java
@@ -5,8 +5,8 @@ import javax.swing.JOptionPane;
 public class TimeException extends Exception{
 	private int hour, minute;
 	private String note = "";
-	public static final String EHOUR = "The hour you have entered is wrong. Try again.";
-	public static final String EMINUTE = "The minute you have entered is wrong. Try again.";
+	public static final String EHOUR = "The hour you have entered is wrong";
+	public static final String EMINUTE = "The minute you have entered is wrong";
 	public TimeException(int hour, int minute){
 		this.hour = hour;
 		this.minute = minute;
@@ -17,10 +17,10 @@ public class TimeException extends Exception{
 	}
 	public void showMessage(){
 		if(note.equals("")){
-			JOptionPane.showMessageDialog(null, "The hour: " + hour + " or the minute: " + minute + " you entered is not possible. Try again.");
+			JOptionPane.showMessageDialog(null, "The hour: " + hour + " or the minute: " + minute + " you entered is not possible.");
 		}
 		else{
-			JOptionPane.showMessageDialog(null, note + ":\n" + "The hour: " + hour + " or the minute: " + minute);
+			JOptionPane.showMessageDialog(null, note + ":\n" + "The hour is: " + hour + "\n The minute is: " + minute);
 		}
 	}
 }

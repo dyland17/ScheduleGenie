49264443b77facd5b03ca10ff33c252eaecc9793
Dylan Dewald <dylan.dewald94@gmail.com>
2017-06-28 11:11:51 -0400

    Created a test class for EditEmployeeScreen.

---
+++ ScheduleGenie/src/dewalddylan/schedulegenie/gui/test/EditEmployeeScreenTest.java
@@ -0,0 +1,10 @@
+package dewalddylan.schedulegenie.gui.test;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import dewalddylan.schedulegenie.data.Employee;
+import dewalddylan.schedulegenie.gui.EditEmployeeScreen;
+
+public class EditEmployeeScreenTest {
+
+}

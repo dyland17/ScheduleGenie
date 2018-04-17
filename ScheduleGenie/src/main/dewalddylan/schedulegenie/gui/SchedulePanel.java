322f6da3acba261e8472c46e788179d2e8c6a925
Dylan Dewald <dylan.dewald94@gmail.com>
2018-04-17 11:20:05 -0400

    Created another offset variable based on amount of employees.

--- ScheduleGenie/src/main/dewalddylan/schedulegenie/gui/SchedulePanel.java
+++ ScheduleGenie/src/main/dewalddylan/schedulegenie/gui/SchedulePanel.java
@@ -11,14 +11,19 @@ import main.dewalddylan.schedulegenie.data.Employee;
 
 public class SchedulePanel extends JPanel{
 	private final Dimension PANELDIM;
-	private final int graphVerticalOffset = 40;
-	private final int graphHorizontalOffset = 160;
-	private final int labTimeOffset = 10;
-	private final int blockWidth = 80;
-	private final int blockHeight = 50;
+	private final int GRAPHVERTICALOFFSET = 40;
+	private final int GRAPHHORIZONTALOFFSET = 160;
+	private final int LABTIMEOFFSET = 10;
+	private final int EMPLOYEEOFFSET = 20;
+	private final int NAMEOFFSET = 20;
+	private final int BLOCKWIDTH = 80;
+	private final int BLOCKHEIGHT = 50;
+	private int amountOfEmployees;
 	public SchedulePanel(){
-		PANELDIM = new Dimension(((blockWidth*24)+graphHorizontalOffset),550);
+		PANELDIM = new Dimension(((BLOCKWIDTH*24)+GRAPHHORIZONTALOFFSET),550);
 		this.setPreferredSize(PANELDIM);
+		//Graph starts out at 9 employee blocks. Will be updated if Employees exceed 9.
+		amountOfEmployees = 9;
 	}
 	public void paintComponent(Graphics g){
 		g.setColor(Color.white);
@@ -28,30 +33,30 @@ public class SchedulePanel extends JPanel{
 			//S.N. the 10 is not set it is a place value for now.
 			//Later, I will implement it based on current number of employees.
 		for(int i = 0; i < 10; i++){
-			g.drawLine(graphHorizontalOffset,((i*blockHeight)+ graphVerticalOffset), getWidth(), (i*blockHeight) + graphVerticalOffset);
+			g.drawLine(GRAPHHORIZONTALOFFSET,((i*BLOCKHEIGHT)+ GRAPHVERTICALOFFSET), getWidth(), (i*BLOCKHEIGHT) + GRAPHVERTICALOFFSET);
 		}
 		//Drawing Time frame
 		for(int x = 0; x < 24; x++){
-			g.drawLine(((x*blockWidth)+ graphHorizontalOffset), graphVerticalOffset, ((x*blockWidth)+graphHorizontalOffset), ((9*blockHeight)+graphVerticalOffset));
+			g.drawLine(((x*BLOCKWIDTH)+ GRAPHHORIZONTALOFFSET), GRAPHVERTICALOFFSET, ((x*BLOCKWIDTH)+GRAPHHORIZONTALOFFSET), ((9*BLOCKHEIGHT)+GRAPHVERTICALOFFSET));
 			if((x+1) >= 12){
 				if((x+1) == 12){
-					g.drawString((x+1) + "pm", ((x*blockWidth)+ graphHorizontalOffset),((10*blockHeight)+labTimeOffset));
+					g.drawString((x+1) + "pm", ((x*BLOCKWIDTH)+ GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
 					continue;
 				}
 				else if ((x+1) == 24){
-					g.drawString(((x+1)-12) + "am", ((x*blockWidth)+ graphHorizontalOffset),((10*blockHeight)+labTimeOffset));
+					g.drawString(((x+1)-12) + "am", ((x*BLOCKWIDTH)+ GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
 					continue;
 				}
-				g.drawString(((x+1)-12) + "pm", ((x*blockWidth)+graphHorizontalOffset),((10*blockHeight)+labTimeOffset));
+				g.drawString(((x+1)-12) + "pm", ((x*BLOCKWIDTH)+GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
 			}
 			else{
-				g.drawString((x+1) + "am", ((x*blockWidth)+graphHorizontalOffset),((10*blockHeight)+labTimeOffset));
+				g.drawString((x+1) + "am", ((x*BLOCKWIDTH)+GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
 			}
 		}
 		int y = 1;
 		//Write names in
 		for(Employee employee: ScheduleScreen.employeeMonitor.getEmployeeList()){
-			g.drawString(employee.getFirstName() + " " + employee.getLastName(), 0, y*blockHeight);
+			g.drawString(employee.getFirstName() + " " + employee.getLastName(), EMPLOYEEOFFSET, y*BLOCKHEIGHT + NAMEOFFSET);
 			y++;
 		}
 	}

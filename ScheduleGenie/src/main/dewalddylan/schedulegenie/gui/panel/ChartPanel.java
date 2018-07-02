5fdd2b7b122018b7aeb7331dc6b358a51e7e6c31
Dylan Dewald <dylan.dewald94@gmail.com>
2018-07-02 13:56:39 -0400

    Little changes to ChartPanel that are still being worked on.

--- ScheduleGenie/src/main/dewalddylan/schedulegenie/gui/panel/ChartPanel.java
+++ ScheduleGenie/src/main/dewalddylan/schedulegenie/gui/panel/ChartPanel.java
@@ -3,6 +3,7 @@ package main.dewalddylan.schedulegenie.gui.panel;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -10,27 +11,29 @@ import main.dewalddylan.schedulegenie.data.names.GUIDim;
 
 public class ChartPanel extends Panel implements MouseListener{
 	private final int GRAPHVERTICALOFFSET = 20;
-	private static final int GRAPHHORIZONTALOFFSET = 100;
+	private static final int GRAPHHORIZONTALOFFSET = 120;
 	private final int LABTIMEOFFSET = 10;
 	private final int EMPLOYEEOFFSET = 20;
 	private final int NAMEOFFSET = 20;
 	private static final int BLOCKWIDTH = 30;
 	private static final int BLOCKHEIGHT = 50;
 	private int amountOfEmployees;
-	private int panelXPoint;
-	private int panelYPoint;
+	private int viewXPoint;
+	private int viewYPoint;
 	public ChartPanel(){
 		super(GUIDim.MAINPANELDIM,Panel.LOWERBORDER);
 		//Graph starts out at 9 employee blocks. Will be updated if Employees exceed 9.
 		amountOfEmployees = 9;
-		panelXPoint = 0;
-		panelYPoint = 0;
+		viewXPoint = 0;
+		viewYPoint = 0;
+		init();
 	}
 	@Override
 	public void paintComponent(Graphics g){
-		g.setColor(Color.white);
-		g.fillRect(0, 0, getWidth(), getHeight());
-		g.setColor(Color.black);
+		Graphics2D g2d = (Graphics2D) g; 
+		g2d.setColor(Color.white);
+		g2d.fillRect(0, 0, getWidth(), getHeight());
+		g2d.setColor(Color.black);
 		//Horizontal Lines and Vertical Lines
 			//S.N. the 10 is not set it is a place value for now.
 			//Later, I will implement it based on current number of employees.
@@ -39,20 +42,20 @@ public class ChartPanel extends Panel implements MouseListener{
 		}
 		//Drawing Times and vertical lines.
 		for(int x = 6; x < 24; x+=2){
-			g.drawLine((((x - 6)*BLOCKWIDTH)+ GRAPHHORIZONTALOFFSET), GRAPHVERTICALOFFSET, (((x - 6)*BLOCKWIDTH)+GRAPHHORIZONTALOFFSET), ((9*BLOCKHEIGHT)+GRAPHVERTICALOFFSET));
+			g2d.drawLine((((x - 6)*BLOCKWIDTH)+ GRAPHHORIZONTALOFFSET), GRAPHVERTICALOFFSET, (((x - 6)*BLOCKWIDTH)+GRAPHHORIZONTALOFFSET), ((9*BLOCKHEIGHT)+GRAPHVERTICALOFFSET));
 			if(x  >= 12){
 				if(x == 12){
-					g.drawString((x) + "pm", (((x - 6)*BLOCKWIDTH)+ GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
+					g2d.drawString((x) + "pm", (((x - 6)*BLOCKWIDTH)+ GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
 					continue;
 				}
 				else if (x == 24){
-					g.drawString((x-12) + "am", (((x - 6)*BLOCKWIDTH)+ GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
+					g2d.drawString((x-12) + "am", (((x - 6)*BLOCKWIDTH)+ GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
 					continue;
 				}
-				g.drawString(((x) -12) + "pm", (((x - 6)*BLOCKWIDTH)+GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
+				g2d.drawString(((x) -12) + "pm", (((x - 6)*BLOCKWIDTH)+GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
 			}
 			else{
-				g.drawString(x + "am", (((x - 6)*BLOCKWIDTH)+GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
+				g2d.drawString(x + "am", (((x - 6)*BLOCKWIDTH)+GRAPHHORIZONTALOFFSET),(((amountOfEmployees + 1)*BLOCKHEIGHT)+LABTIMEOFFSET));
 			}
 		}
 		int y = 1;
@@ -71,7 +74,7 @@ public class ChartPanel extends Panel implements MouseListener{
 
 	@Override
 	protected void init() {
-		// TODO Auto-generated method stub
+		this.addMouseListener(this);
 		
 	}
 	@Override

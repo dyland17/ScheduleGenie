bb936ffccedca36b91b99b34ca8e97b28c0e78cb
Dylan Dewald <dylan.dewald94@gmail.com>
2018-07-23 20:37:10 -0400

    Forgot to change indent to offset name.

--- ScheduleGenie/src/main/dewalddylan/schedulegenie/data/Position.java
+++ ScheduleGenie/src/main/dewalddylan/schedulegenie/data/Position.java
@@ -3,51 +3,52 @@ package main.dewalddylan.schedulegenie.data;
 public class Position {
 	private final int xPos;
 	private final int yPos;
-	private final int xIndent;
-	private final int yIndent;
+	private final int xOffset;
+	private final int yOffset;
 	/**
 	 * 
 	 * @param xPos position of x coordinate.
 	 * @param yPos position of y coordinate.
-	 * @param indent indentation of x and y positions.
+	 * @param offset for x and y positions.
 	 */
-	public Position(int xPos, int yPos, int indent){
+	public Position(int xPos, int yPos, int offset){
 		this.xPos = xPos;
 		this.yPos = yPos;
-		xIndent = indent;
-		yIndent = indent;
+		this.xOffset = offset;
+		this.yOffset = offset;
 	}
 	/**
 	 * 
 	 * @param xPos position of x coordinate.
 	 * @param yPos position of y coordinate.
-	 * @param xIndent indentation of x position.
+	 * @param xOffset offset of x position.
+	 * @param yOffset offset of y position.
 	 */
-	public Position(int xPos, int yPos, int xIndent, int yIndent) {
+	public Position(int xPos, int yPos, int xOffset, int yOffset) {
 		this.xPos = xPos;
 		this.yPos = yPos;
-		this.xIndent = xIndent;
-		this.yIndent = yIndent;
+		this.xOffset = xOffset;
+		this.yOffset = yOffset;
 	}
 	/**
 	 * 
-	 * @return xPos plus xIndent if a indent exist.
+	 * @return xPos plus xOffset if an offset exists.
 	 */
 	public int xPos() {
-		return (xPos + xIndent);
+		return (xPos + xOffset);
 	}
 	/**
 	 * 
-	 * @return yPos plus yIndent if a indent exist.
+	 * @return yPos plus yOffset if an offset exists.
 	 */
 	public int yPos() {
-		return (yPos + yIndent);
+		return (yPos + yOffset);
 	}
-	public int getxIndent() {
-		return xIndent;
+	public int xOffset() {
+		return xOffset;
 	}
-	public int getyIndent() {
-		return yIndent;
+	public int yOffset() {
+		return yOffset;
 	}
 	/**
 	 * @param xPos new xPos for Position object.
@@ -55,31 +56,31 @@ public class Position {
 	 * @return new instance of the Position object.
 	 * <h2>Note:
 	 * 			<p>
-	 * 				  xIndent: value is equal to the previous instance of the object. <br>
-	 * 			 	  yIndent: value is equal to the previous instance of the object.
+	 * 				  xOffset: value is equal to the previous instance of the object. <br>
+	 * 			 	  yOffset: value is equal to the previous instance of the object.
 	 * 			</p>
 	 * <h2/>
 	 */
 	public Position newInstance(int xPos, int yPos){
-		return new Position(xPos, yPos, this.xIndent, this.yIndent);
+		return new Position(xPos, yPos, this.xOffset, this.yOffset);
 	}
 	/**
 	 * @param xPos new xPos for Position object.
 	 * @param yPos new yPos for Position object.
-	 * @param indent x and y indentation are equal to indent value.
+	 * @param offset x and y are equal to the offset value.
 	 * @return new instance of the Position object.
 	 */
-	public Position newInstance(int xPos, int yPos, int indent){
-		return new Position(xPos, yPos, indent);
+	public Position newInstance(int xPos, int yPos, int offset){
+		return new Position(xPos, yPos, offset);
 	}
 	/**
 	 * @param xPos new xPos for Position object.
 	 * @param yPos new yPos for Position object.
-	 * @param xIndent x indentation.
-	 * @param yIndent y indentation.
+	 * @param xOffset x offset.
+	 * @param yOffset y offset.
 	 * @return new instance of the Position object.
 	 */
-	public Position newInstance(int xPos, int yPos, int xIndent, int yIndent){
-		return new Position(xPos, yPos, xIndent, yIndent);
+	public Position newInstance(int xPos, int yPos, int xOffset, int yOffset){
+		return new Position(xPos, yPos, xOffset, yOffset);
 	}
 }
